bda1ae99fd354b2b4d6bf28773278db6eeaeacd5
joetidee <joe@saeker.co.uk>
2017-06-16 02:26:30 +0100

    ...

--- todoapp/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from '../src/app.js';
-import initialState from './initialState.js';
-import reducer from './reducer.js';
-
-let store = createStore(reducer, initialState);
-
-export default store;
